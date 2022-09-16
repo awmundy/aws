dcd5c4496ab499941e4617262155d6a36783c250
Andrew Mundy <awmundy@gmail.com>
2022-09-16 15:03:50 -0400

    todos: aws services to try

---
+++ todos.py
@@ -0,0 +1,31 @@
+# cloudtrail to track actions
+#   - console x
+#   - python
+# figure out best way to summarize cloudtrail logs
+# set up IAM role as admin user, try doing everything as that IAM
+#   - console x
+#   - python
+# grant admin user the ability to see billing stuff
+#   - console x
+# make s3 bucket
+#   - console x
+#   - python
+# cloudwatch: within cloudtrail log settings, set cloudwatch to track it
+#   - console x
+#   - python
+# create cost and usage report
+#   - console x
+# set billing alarm
+#   - console
+# use aws config to see what all my settings are (costs money)
+# create and use EC2 instance, free tier first
+#   - try setting up with gui
+#   - needs to be stored in an s3 bucket, so make that first
+# set billing alarm
+#   - console
+#   - python?
+
+
+
+# maybe glue (but airflow first)
+# try pycharm sdk (seems to have somewhat limited features)
